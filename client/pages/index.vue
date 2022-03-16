bca9163065a3d98be48b0ca32b81f38e5db869a8
Mandrican Andrei <adyta094@gmail.com>
2022-03-17 00:48:45 +0200

    add tailwind navbar

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <Tutorial />
+  <div class="app">
+    <AppNavbar />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import AppNavbar from '~/components/layout/AppNavbar.vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    AppNavbar
+  }
 })
 </script>
