8eb2b19134d28364ca9df646d7cc275a967a1efd
Mandrican Andrei <adyta094@gmail.com>
2022-03-21 20:08:17 +0200

    add packages for server

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+const appServer = require('./app')
+
+const httpServer = http.createServer(appServer)
+
+const PORT = process.env.PORT || 3000
+
+function startServer () {
+  // await mongoConnect()
+
+  httpServer.listen(PORT, () => {
+    console.log('server listening on port, ', PORT)
+  })
+}
+
+startServer()
