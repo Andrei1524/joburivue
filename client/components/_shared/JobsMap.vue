d8a02ac1388d4a04e171272a3f2028f9c77bc39f
Mandrican Andrei <adyta094@gmail.com>
2022-03-26 00:26:46 +0200

    added leaflet opensource map

---
+++ client/components/_shared/JobsMap.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="map-wrap" style="height: 100vh">
+    <client-only>
+      <l-map ref="myMap" :zoom="13" :center="[55.9464418,8.1277591]">
+        <l-tile-layer url="https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=jptvrcCyLPSDDAHfJvLx" />
+        <l-marker :lat-lng="[55.9464418,8.1277591]" />
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'JobsMap',
+
+  data () {
+    return {}
+  },
+
+  head () {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css'
+        }
+      ]
+    }
+  },
+
+  mounted () {
+    this.$nextTick(() => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log(position.coords.longitude)
+          this.$refs.myMap.mapObject.setView([position.coords.latitude, position.coords.longitude], 8)
+        })
+      } else {
+        // Geolocation is not supported by this browser.
+      }
+    })
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import './design/variables';
+
+</style>
