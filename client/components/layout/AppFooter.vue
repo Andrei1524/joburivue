67450d17ad286dbc992b1cbe21294f4c7825074d
Mandrican Andrei <adyta094@gmail.com>
2022-03-29 22:38:50 +0300

    add footer component

---
+++ client/components/layout/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    footer
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AppFooter',
+
+  data () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import './design/variables';
+</style>
