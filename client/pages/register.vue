3a2ae3ba50c309186b46793c3273b4ad050e2990
Mandrican Andrei <adyta094@gmail.com>
2022-04-02 13:04:24 +0300

    Update register.vue

--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="register-page">
     <h1 class="title is-1 has-text-centered mt-6">
-      Intră în contul tău
+      Creează-ti un cont!
     </h1>
     <div class="container is-max-desktop register">
       <div class="box mt-4">
