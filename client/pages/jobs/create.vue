f7bcc18d4baa95189db090f1e3b47f0cf08fd4eb
Mandrican Andrei <adyta094@gmail.com>
2022-04-03 21:23:00 +0300

    Update create.vue

--- client/pages/jobs/create.vue
+++ client/pages/jobs/create.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="create-job">
-    create job
-  </div>
+  <div class="create-job">create job</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'JobCreate',
-  components: {
-  },
-  middleware: 'auth'
-})
+  name: "JobCreate",
+  components: {},
+  middleware: "auth",
+});
 </script>
 
 <style lang="scss" scoped>
-@import './design/variables';
-
+@import "./design/variables";
 </style>
