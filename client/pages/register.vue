b97238768fcdbd1ccb746a3f99daf30b402a66f0
Mandrican Andrei <adyta094@gmail.com>
2022-04-03 21:22:54 +0300

    prettify register

--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,25 +1,14 @@
 <template>
   <div class="register-page">
-    <h1 class="title is-1 has-text-centered mt-6">
-      Creează-ti un cont!
-    </h1>
+    <h1 class="title is-1 has-text-centered mt-6">Creează-ti un cont!</h1>
     <div class="container is-max-desktop register">
       <div class="box mt-4">
         <div class="register">
-          <b-field
-            label="Nume"
-            :message="''"
-          >
-            <b-input
-              v-model="name"
-              placeholder="ex: john doe"
-            />
+          <b-field label="Nume" :message="''">
+            <b-input v-model="name" placeholder="ex: john doe" />
           </b-field>
 
-          <b-field
-            label="Email"
-            :message="''"
-          >
+          <b-field label="Email" :message="''">
             <b-input
               v-model="email"
               placeholder="ex: your@gmail.com"
@@ -27,10 +16,7 @@
             />
           </b-field>
 
-          <b-field
-            label="Parola"
-            :message="''"
-          >
+          <b-field label="Parola" :message="''">
             <b-input
               v-model="password"
               placeholder="123456"
@@ -39,10 +25,7 @@
             />
           </b-field>
 
-          <b-field
-            label="Confirmă Parola"
-            :message="''"
-          >
+          <b-field label="Confirmă Parola" :message="''">
             <b-input
               v-model="passwordConfirm"
               placeholder="123456"
@@ -52,7 +35,8 @@
 
           <b-field class="mt-5">
             <b-checkbox v-model="weeklyJobAlerts">
-              Trimite-mi săptămânal alerte de locuri de muncă și alte e-mailuri legate de Vue.js
+              Trimite-mi săptămânal alerte de locuri de muncă și alte e-mailuri
+              legate de Vue.js
             </b-checkbox>
           </b-field>
 
@@ -72,25 +56,25 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'AppLogin',
+  name: "AppLogin",
 
-  data () {
+  data() {
     return {
-      name: '',
-      email: '',
-      password: '',
-      passwordConfirm: '',
-      weeklyJobAlerts: false
-    }
-  }
-})
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      weeklyJobAlerts: false,
+    };
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-@import './design/variables';
+@import "./design/variables";
 
 .register {
   max-width: 400px;
