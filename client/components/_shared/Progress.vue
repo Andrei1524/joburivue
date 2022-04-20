ceb95bad3f5dadd490d10cee4e3b1292a97979b1
Mandrican Andrei <adyta094@gmail.com>
2022-04-20 16:59:41 +0300

    remove old progress

--- client/components/_shared/Progress.vue
+++ client/components/_shared/Progress.vue
@@ -1,15 +1,9 @@
 <template>
-  <ul class="process-steps is-flex is-justify-content-space-between">
-    <li
-      v-for="step in steps"
-      :key="step.title"
-      class="is-flex is-flex-direction-column is-align-items-center"
-    >
-      <b-icon :icon="step.icon" size="is-medium" />
-      <span>{{ step.title }}</span>
-      <span class="li-bottom"></span>
-    </li>
-  </ul>
+  <b-steps>
+    <b-step-item label="Account" icon="account-key"></b-step-item>
+    <b-step-item label="Profile" icon="account"></b-step-item>
+    <b-step-item label="Social" icon="account-plus"></b-step-item>
+  </b-steps>
 </template>
 
 <script lang="ts">
@@ -29,87 +23,10 @@ export default Vue.extend({
     },
   },
 
-  watch: {
-    currentStep() {
-      this.initProgress();
-    },
-  },
-
-  mounted() {
-    this.initProgress();
-  },
-
-  methods: {
-    // TODO: handle style bottom line of progress
-    initProgress() {
-      const maxWidth = 600;
-      const liList = document.querySelectorAll(".process-steps li");
-      const limit = liList.length - 1;
-      let liItem: any = null;
-      let lineSpan: any = null;
-      const stepsToFill = Array.from(Array(this.steps.length).keys());
-      const step = this.currentStep - 1;
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      liList.forEach((li, i) => {
-        liItem = liList.item(i);
-        if (i < limit) {
-          const liWidth = liItem.getBoundingClientRect().width;
-          lineSpan = document.createElement("span");
-          lineSpan.classList.add("line");
-          lineSpan.style.width =
-            maxWidth / 2 - liWidth - liWidth / 2 - 6 + "px";
-          lineSpan.style.left = liWidth + 2.5 + "px";
-
-          liItem.appendChild(lineSpan);
-        }
-        if (i <= step) {
-          if (stepsToFill.includes(i)) {
-            liItem.style.color = "#D9665B";
-            lineSpan.style.background =
-              "linear-gradient(90deg, #D9665B, #D9665B)";
-            lineSpan.style.opacity = "0.9";
-          }
-        }
-      });
-    },
-  },
+  mounted() {},
 });
 </script>
 
 <style lang="scss" scoped>
 @import "./design/variables";
-
-.process-steps {
-  max-width: 600px;
-  margin: 0 auto;
-
-  li {
-    position: relative;
-    width: 72px;
-    height: 72px;
-    box-sizing: border-box;
-    justify-content: center;
-
-    .li-bottom {
-      height: 4px;
-      width: 100%;
-      display: block;
-      background-color: #bbbec4;
-      position: absolute;
-      bottom: 0;
-      z-index: 2;
-    }
-  }
-}
-
-::v-deep .line {
-  height: 6px;
-  background: linear-gradient(90deg, #5c627354, #5c6273);
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  opacity: 0.4;
-  border-radius: 52px;
-}
 </style>
