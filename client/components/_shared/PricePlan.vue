25ff99454d1789c886bfbe09acabc2e1992085ad
Mandrican Andrei <adyta094@gmail.com>
2022-04-27 20:55:26 +0300

    create PricePlan componenet + add it to pricing page

---
+++ client/components/_shared/PricePlan.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>price plan</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "PricePlan",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./design/variables";
+</style>
--- client/pages/pricing/index.vue
+++ client/pages/pricing/index.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="pricing-page hero is-success position-relative">
-    <div class="hero-wrapper" />
-    <!-- Hero content: will be in the middle -->
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <p class="title">Lista servicii si beneficii</p>
-        <p class="subtitle mt-2">Alege un plan care ti se potriveste</p>
+  <div class="pricing-page">
+    <section class="hero is-success position-relative">
+      <div class="hero-wrapper" />
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <p class="title">Lista servicii si beneficii</p>
+          <p class="subtitle mt-2">Alege un plan care ti se potriveste</p>
+        </div>
       </div>
+    </section>
+
+    <div class="price-plans container is-max-desktop mt-6">
+      <PricePlan />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import PricePlan from "~/components/_shared/PricePlan.vue";
 
 export default Vue.extend({
   name: "AppPricing",
+  components: {
+    PricePlan,
+  },
 
   data() {
     return {};
