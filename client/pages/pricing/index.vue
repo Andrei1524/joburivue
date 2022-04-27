4c15c5a4592a057822071c8f1da8f25d648ec8e6
Mandrican Andrei <adyta094@gmail.com>
2022-04-27 20:27:14 +0300

    added pricing page

---
+++ client/pages/pricing/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pricing-page">pricing</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AppPricing",
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./design/variables";
+</style>
