cf1e935cd98bededcc0d2dd8de3613827ccf1bfb
Mandrican Andrei <adyta094@gmail.com>
2022-04-27 20:42:46 +0300

    added hero

--- client/pages/pricing/index.vue
+++ client/pages/pricing/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="pricing-page">pricing</div>
+  <div class="pricing-page hero is-success position-relative">
+    <div class="hero-wrapper" />
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <p class="title">Lista servicii si beneficii</p>
+        <p class="subtitle mt-2">Alege un plan care ti se potriveste</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,4 +27,18 @@ export default Vue.extend({
 
 <style lang="scss" scoped>
 @import "./design/variables";
+
+.hero-wrapper {
+  background: #212940d9;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+}
+
+.hero-body {
+  background: url("~assets/hero.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
 </style>
