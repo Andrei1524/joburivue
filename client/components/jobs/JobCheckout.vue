488b59e69b0c356b73755aa153fae7e5db7e690f
Mandrican Andrei <adyta094@gmail.com>
2022-04-28 21:18:06 +0300

    add job checkout comp

---
+++ client/components/jobs/JobCheckout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="job-checkout">checkout</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import _ from "lodash";
+
+export default Vue.extend({
+  name: "JobCheckout",
+  middleware: "auth",
+
+  data() {
+    return {};
+  },
+
+  async fetch() {},
+
+  computed: {},
+
+  methods: {},
+});
+</script>
+
+<style lang="scss" scoped>
+@import "./design/variables";
+</style>
