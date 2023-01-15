338e8950c2860278c51e7bb304cb36323b5f2934
Mandrican Andrei <adyta094@gmail.com>
2023-01-15 18:44:04 +0200

    add company schema

--- server/src/model/company.model.ts
+++ server/src/model/company.model.ts
@@ -1,1 +1,16 @@
-// TODO: to be done
+import { Schema, model } from 'mongoose';
+import { CompanyInterface } from '../ts/interfaces/company.interfaces';
+
+const CompanySchema = new Schema<CompanyInterface>(
+  {
+    name: { type: String, required: true },
+    logo: { type: String, required: false },
+    website: { type: String, required: false },
+    description: { type: String, required: true },
+  },
+
+  { timestamps: true }
+);
+
+const Company = model<CompanyInterface>('Company', CompanySchema);
+export = Company;
