9870146a566c319ec71c38901440a114b1d9fb50
Mandrican Andrei <adyta094@gmail.com>
2023-09-10 22:32:30 +0300

    fix URI string accessing wrong DB

--- server/src/services/_mongo.service.ts
+++ server/src/services/_mongo.service.ts
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connection.once('open', () => {
+mongoose.connection.once("open", () => {
   // event listener for connection
-  console.log('mongo db connection ready');
+  console.log("mongo db connection ready");
 });
 
-mongoose.connection.on('error', (error: Error) => {
+mongoose.connection.on("error", (error: Error) => {
   // event listener for connection, we put this anywhere we want
   console.error(error);
 });
